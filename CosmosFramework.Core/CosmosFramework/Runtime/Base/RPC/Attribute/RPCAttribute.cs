d80ce6947a1fa59b2ef4db1d19ba04d8afc7bb7e
Don <zhengdonn@gmail.com>
2021-10-18 16:07:10 +0800

    Update：移除RPC；

--- CosmosFramework.Core/CosmosFramework/Runtime/Base/RPC/Attribute/RPCAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-namespace Cosmos.RPC
-{
-    [AttributeUsage(AttributeTargets.Method,AllowMultiple =false,Inherited =false)]
-    public class RPCAttribute:Attribute{}
-}
