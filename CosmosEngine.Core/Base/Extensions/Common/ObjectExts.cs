fc3a63774aa361f7587dec76c4ca0b13f363bc58
Donn <44013616+DonnYep@users.noreply.github.com>
2023-02-17 11:33:38 +0800

    Update: 新增反射扩展

--- CosmosEngine.Core/Base/Extensions/Common/ObjectExts.cs
+++ CosmosEngine.Core/Base/Extensions/Common/ObjectExts.cs
@@ -1,4 +1,7 @@
-﻿namespace Cosmos
+﻿using System.Linq;
+using System.Reflection;
+
+namespace Cosmos
 {
     public static class ObjectExts
     {
@@ -9,6 +12,20 @@
         public static T CastTo<T>(this object @this) where T : class
         {
             return (T)@this;
+        }
+        public static T InvokeMethod<T>(this object obj, string methodName, object[] args)
+        {
+            return (T)obj.GetType().GetMethod(methodName, args.Select(o => o.GetType()).ToArray()).Invoke(obj, args);
+        }
+        public static void InvokeMethod(this object obj, string methodName, object[] args)
+        {
+            var type = obj.GetType();
+            type.GetMethod(methodName, args.Select(o => o.GetType()).ToArray()).Invoke(obj, args);
+        }
+        public static FieldInfo[] GetFields(this object obj)
+        {
+            FieldInfo[] fieldInfos = obj.GetType().GetFields(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            return fieldInfos;
         }
     }
 }
