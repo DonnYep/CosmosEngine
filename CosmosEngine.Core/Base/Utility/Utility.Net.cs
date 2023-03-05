e6f0f96f5510803599be8a1a00330c3503136d5b
Donn <44013616+DonnYep@users.noreply.github.com>
2023-03-05 20:42:11 +0800

    Update: 更新net工具类

--- CosmosEngine.Core/Base/Utility/Utility.Net.cs
+++ CosmosEngine.Core/Base/Utility/Utility.Net.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
@@ -152,6 +153,46 @@ namespace Cosmos
                     }
                 }
                 throw new Exception("No network adapters with an IPv6 address in the system!");
+            }
+            public static bool IsPrivateIP(IPAddress ip)
+            {
+                if (IPAddress.IsLoopback(ip)) return true;
+                ip = ip.IsIPv4MappedToIPv6 ? ip.MapToIPv4() : ip;
+                byte[] bytes = ip.GetAddressBytes();
+                return ip.AddressFamily switch
+                {
+                    AddressFamily.InterNetwork when bytes[0] == 10 => true,
+                    AddressFamily.InterNetwork when bytes[0] == 100 && bytes[1] >= 64 && bytes[1] <= 127 => true,
+                    AddressFamily.InterNetwork when bytes[0] == 169 && bytes[1] == 254 => true,
+                    AddressFamily.InterNetwork when bytes[0] == 172 && bytes[1] == 16 => true,
+                    AddressFamily.InterNetwork when bytes[0] == 192 && bytes[1] == 88 && bytes[2] == 99 => true,
+                    AddressFamily.InterNetwork when bytes[0] == 192 && bytes[1] == 168 => true,
+                    AddressFamily.InterNetwork when bytes[0] == 198 && bytes[1] == 18 => true,
+                    AddressFamily.InterNetwork when bytes[0] == 198 && bytes[1] == 51 && bytes[2] == 100 => true,
+                    AddressFamily.InterNetwork when bytes[0] == 203 && bytes[1] == 0 && bytes[2] == 113 => true,
+                    AddressFamily.InterNetwork when bytes[0] >= 233 => true,
+                    AddressFamily.InterNetworkV6 when ip.IsIPv6Teredo || ip.IsIPv6LinkLocal || ip.IsIPv6Multicast || ip.IsIPv6SiteLocal || bytes[0] == 0 || bytes[0] >= 252 => true,
+                    _ => false
+                };
+            }
+            public static Uri MatchUrl(string s, out bool isMatch)
+            {
+                try
+                {
+                    var uri = new Uri(s);
+                    isMatch = Dns.GetHostAddresses(uri.Host).Any(ip => !IsPrivateIP(ip));
+                    return uri;
+                }
+                catch
+                {
+                    isMatch = false;
+                    return null;
+                }
+            }
+            public static bool MatchUrl(string s)
+            {
+                MatchUrl(s, out var isMatch);
+                return isMatch;
             }
         }
     }
